2551c87c459618747b0c2e3e9723ae679892b747
dalancoder <mustafadalan0@gmail.com>
2024-07-30 13:07:31 +0300

    son

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const body = document.querySelector("body");
 const colorText = document.getElementById("colorText");
+const input = document.querySelector("#colorInput");
 const click = document.querySelector(".btn-click");
 const mouseOver = document.querySelector(".btn-over");
+const copyIcon = document.getElementById("copy");
 
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
@@ -29,4 +31,19 @@ click.addEventListener("click", function () {
 mouseOver.addEventListener("mouseover", function () {
   body.style.backgroundColor = getRandomColor();
   colorText.textContent = getRandomColor();
+});
+
+input.addEventListener("input", function () {
+  body.style.backgroundColor = input.value;
+  colorText.textContent = input.value;
+}); //input olayı, kullanıcı bir form elemanına veri girdiğinde, veriyi değiştirdiğinde veya seçtiğinde tetiklenir. Bu olay, veri değişikliği anında tetiklenir, yani kullanıcı her veri girişi yaptığında veya seçimde bulunduğunda çalışır.
+
+body.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    body.style.backgroundColor = getRandomColor();
+  }
+});
+
+copyIcon.addEventListener("click", function () {
+  navigator.clipboard.writeText(input.value);
 });
